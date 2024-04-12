501b4c8710792051678ad2821895bd8b5f1235c3
unknown <seraeben@gmail.com>
2024-04-12 15:07:42 -0400

    project details v3

--- src/components/Pro/FeaturedPro.jsx
+++ src/components/Pro/FeaturedPro.jsx
@@ -28,21 +28,23 @@ class FeaturedPro extends React.Component {
             <br/>
             <div className='proj-wrapper'>
                 <div className='proj-content'>
-                    <div>
+                    <div className='proj-photos'>
                         {this.getPhotos(selectedProj, photos)}
                     </div>
-                    <div>
-                        {client && <h3>Client</h3>}
-                        {client && <p>{client}</p>}
-                        {role && <h3>My Role</h3>}
-                        {role && <p>{role}</p>}
-                        {story && <h3>Summary</h3>}
-                        {story && <p>{story}</p>}
-                        {tools && <h3>Tools</h3>}
-                        {tools && <p>{tools}</p>}
-                        {hardskills && <h3>Skills</h3>}
-                        {hardskills && <p>{`${hardskills}${softskills ? `, ${softskills}` : ''}`}</p>}
-                        <a className='btn card_btn' href={!isHosted ? url : require(`/public/projs/${id}/${url}`)} target='_blank' rel='noreferrer'>
+                    <div className='proj-inner-content'>
+                        <div>
+                            {client && <h3>Client</h3>}
+                            {client && <p>{client}</p>}
+                            {role && <h3>My Role</h3>}
+                            {role && <p>{role}</p>}
+                            {story && <h3>Summary</h3>}
+                            {story && <p>{story}</p>}
+                            {tools && <h3>Tools</h3>}
+                            {tools && <p>{tools}</p>}
+                            {hardskills && <h3>Skills</h3>}
+                            {hardskills && <p>{`${hardskills}${softskills ? `, ${softskills}` : ''}`}</p>}
+                        </div>
+                        <a className='btn card-btn' href={!isHosted ? url : require(`/public/projs/${id}/${url}`)} target='_blank' rel='noreferrer'>
                             <span>View Source</span>
                         </a>
                     </div>
