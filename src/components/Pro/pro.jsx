e31675ae4059e20d84db4de42d2cf8fff560986d
unknown <seraeben@gmail.com>
2024-04-15 14:57:11 -0400

    bug fixes

--- src/components/Pro/pro.jsx
+++ src/components/Pro/pro.jsx
@@ -13,6 +13,8 @@ const Pro = () => {
   const [isLoaded, setIsLoaded] = useState(false)
   const [data, setData] = useState(null)
   const [selectedProj, setSelectedProj] = useState(null)
+  const [selectedProjId, setSelectedProjId] = useState('site')
+  const [showSelectedProj, setShowSelectedProj] = useState(false)
 
   const loadSettings = () => {
     $.getJSON(endpointGetProjects, (datajson) => setData(datajson))
@@ -21,15 +23,21 @@ const Pro = () => {
   useEffect(() => {
     setIsLoaded(false)
     loadSettings()
-    setTimeout(() => { setIsLoaded(true) }, 1600)
-    setTimeout(() => { document.getElementById(selectedProj ? selectedProj.id : 'root').scrollIntoView({ behavior: 'smooth'}) }, 1700)
+    setTimeout(() => {
+      setIsLoaded(true)
+    }, 1600)
+
+    setShowSelectedProj(!!selectedProj)
   }, [selectedProj])
 
   useEffect(() => {
-  }, [selectedProj])
+    const id = selectedProjId || 'site'
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth'})
+  }, [selectedProjId])
 
-  const selectPro = (selectedPro) => {
-    setSelectedProj(selectedPro)
+  const selectPro = (pro) => {
+    setSelectedProj(pro)
+    if (pro) setSelectedProjId(pro.id)
   }
   
   if (!isLoaded) return <Loader />
@@ -38,7 +46,7 @@ const Pro = () => {
     <Fragment>
       <Nav />
       {data && <main>
-        {selectedProj 
+        {selectedProj && showSelectedProj 
         ? <FeaturedPro selectedProj={selectedProj} selectPro={selectPro}/> 
         : <div>
             <h2>Portfolio</h2>
