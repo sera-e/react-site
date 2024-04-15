40fc4d476d7463edf163d62c06bb20171f731541
unknown <seraeben@gmail.com>
2024-04-15 15:07:32 -0400

    bug fixes

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -3,8 +3,8 @@ import { nav, toggleMenu } from '../utils'
 
 import './nav.css'
 
-const Nav = () => <nav className='top'>
-    <div>
+const Nav = ({ IsLoading }) => <nav className='top'>
+    <div className={IsLoading ? 'loading' : 'loading loaded'}>
         <button className='menu' onClick={() => { toggleMenu() }}><i className='fas fa-bars'></i>Menu</button>
         <button className='close' onClick={() => { toggleMenu() }}><i className='fas fa-times'></i>Close</button>
         <ul>
