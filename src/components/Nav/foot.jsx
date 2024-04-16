630c25ca77a0443bbc90d7e10b03c250b75735b4
unknown <seraeben@gmail.com>
2024-04-16 05:49:20 -0400

    css

--- src/components/Nav/foot.jsx
+++ src/components/Nav/foot.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './nav.css'
 
-const Footer = () => <footer>seraeben©2024</footer>
+const Footer = () => <footer>
+    <div>
+        Powered by <i className='fab fa-react' />
+    </div>
+    seraeben©2024
+    </footer>
 
 export default Footer
