551aa2b9714eab619a093839fcd9011ab01466be
unknown <seraeben@gmail.com>
2024-04-18 06:01:07 -0400

    resume

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -22,7 +22,12 @@ const About = () => {
           <h2>About me</h2>
           <hr />
           <div>
-            <img className='about-img' src={require('./me.jpg')} alt={`me`} />
+            <div className='about-img'>
+              <img src={require('./me.jpg')} alt={`me`} />
+              <Link className='btn card-btn' to='https://docs.google.com/document/d/1jVhgV5iM3fHNR2dNnOsHP3ovK5ymPhe4wuDt6txHDHA/edit?usp=sharing' target='_blank'>
+                View My Resume <i className='fa-light fa-file-user' />
+              </Link>
+            </div>
             <div className='about-txt'>
               <p>
                 Hello, I'm Sera :)
@@ -32,11 +37,8 @@ const About = () => {
                 These days, I primarily do UI/UX development though, I have both experience and a background in web design, graphic design, photography, videography, and media editing.
                 I've always been &#10024; a creative &#10024; but everything I learned came from leaning into my kinesthetic learning style and my knack of paying attention to the little details.
                 <br /><br />
-                At this point in my career, I find myself using everything I've learned to guide others, while still finding more ways to grow myself, because there's always something new to learn.
+                At this point in my career, I find myself using everything I've learned to guide others, while still finding new ways to learn and grow.
               </p>
-              <Link className='btn card-btn' to='https://docs.google.com/document/d/1jVhgV5iM3fHNR2dNnOsHP3ovK5ymPhe4wuDt6txHDHA/edit?usp=sharing' target='_blank'>
-                View My Resume <i className='fa-light fa-file-user' />
-              </Link>
             </div>
           </div>
         </div>
